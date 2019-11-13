a221a1caf6ea262eb8f5e3f1f8a911e6ef6fefb1
Eric Amrhein <eamrhein@protonmail.ch>
2019-11-13 15:37:39 -0800

    Update twoSum.js

--- twoSum.js
+++ twoSum.js
@@ -1,6 +1,6 @@
 // Two Number Sum
 //
-// Write a function that takes ina  non-empty array of distinct integers and an integer representing a target sum.
+// Write a function that takes in a  non-empty array of distinct integers and an integer representing a target sum.
 // If any two numbers in the input array sum up ot the target sum, the function should return them in an array, in sorted
 // order. If no two number sum up to the target sum, the function should return an empty array. Assume that there will be
 // at most one pair of numbers summing up to the target sum.
