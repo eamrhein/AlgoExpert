35b5ef9f5954a1eb9abbba224ceafea04f7bd10e
Eric Amrhein <eamrhein@protonmail.ch>
2019-11-15 16:32:56 -0800

    Create BST.js
    Stills needs to be fixed

---
+++ BST.js
@@ -0,0 +1,65 @@
+class BST {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+
+  insert(value) {
+    // Write your code here.
+			if (value < this.value) {
+				if(this.left === null) {
+					this.left = new BST(value)
+				} else {
+					this.left.insert(value)
+				}
+			}
+			if (value > this.value) {
+				if (this.right === null) {
+					this.right = new BST(value);
+				} else {
+					this.right.insert(value)
+				}
+			}
+		
+    return this;
+  }
+
+  contains(value) {
+		if (this.value === value) return true;
+		if (value < this.value) {
+			if (this.left === null) {
+				return false;
+			}
+			return this.left.contains(value);
+		} 
+		if  (value > this.value) {
+			if (this.right === null) {
+				return false
+			}
+			return this.right.contains(value)
+		}
+		return false
+		// // Write your code here.
+  }
+
+  remove(value, parent = null) {
+		// Write your code here.
+		// Do not edit the return statement of this method.
+		if (value < this.value) {
+			if (this.left) {
+				this.left.remove(value, this);
+			}
+		} else if  (value > this.value) {
+			if (this.right) {
+				this.right.remove(value, this)
+			}
+		} else {
+			if (this.left && this.right) {
+				// do something
+			} else if (parent === null) {
+				
+			}
+		}
+    return this;
+  }
